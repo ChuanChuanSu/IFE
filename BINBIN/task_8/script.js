7590a151505d5a3c7595a29e113996bc1eb7b701
Leo <1597842725@qq.com>
2017-07-06 10:28:37 +0800

    task_8 final

--- BINBIN/task_8/script.js
+++ BINBIN/task_8/script.js
@@ -8,41 +8,71 @@ function bind(fn,obj)
     }
 }
 
+//比bind多传一个参数
+function query(fn,obj)
+{
+    return function(){
+        fn.apply(obj,[document.getElementsByTagName("input")[0].value]);
+    }
+}
+
 function removeColor(){
     this.style.backgroundColor = "white";
 }
 
+//查询成功后执行操作
+function find(){
+    alert("Mission completed!");
+    setTimeout(bind(removeColor,this),500);
+    document.getElementsByTagName("input")[0].value = null;
+}
 
+
+//使用后续遍历查询
 function last(){
+    if(event && event.currentTarget.id == "ergodic")//使用事件对象判断target，若是“遍历”操作，则立即清除input中的内容
+    {
+        document.getElementsByTagName("input")[0].value = null;
+    }
     if (this.previousSibling && this.previousSibling.mvisited)//每次运行第一步去除前一个兄弟节点的背景色
     {
         this.previousSibling.style.backgroundColor = "white";
     }
 
-    if(this.childNodes[1] && !this.childNodes[1].mvisited)//若该节点左子树存在且未访问过，则继续向下寻找最底层左子树，否则执行else
+    if(this.firstElementChild && !this.firstElementChild.mvisited)//若该节点左子树存在且未访问过，则继续向下寻找最底层左子树，否则执行else
     {
-        bind(last,this.childNodes[1])();
+        query(last,this.childNodes[1])();
     }
     else//当前节点为最底层左子树时执行
     {
-
-        this.style.backgroundColor = "yellow";
-        this.mvisited = true;
-
-        if (this.nextSibling && this.id != "super") {
-            setTimeout(last.bind(this.nextSibling), 500);  //0.5s后遍历右子树（兄弟节点）
-        }
-        if (!this.childNodes[1] && !this.nextSibling || this.firstChild.data == "bike" || this.firstChild.data == "program" || this.id == "super")
+        if(arguments[0] == this.firstChild.data)//当执行查询操作，先进行匹配，若匹配成功，则停止查询；否则继续else中的操作
         {
-            setTimeout(bind(removeColor,this),500);  //为某些不方便改变背景色的节点硬绑定removeColor函数
-        }
-        if (this.previousSibling && this.previousSibling.mvisited && !this.nextSibling || this.parentNode.childNodes.length == 2) {
-            setTimeout(bind(last, this.parentNode), 500); //当该节点为最底层右子树时，0.5s后遍历父节点
-        }
-        if(this.id == "super")//遍历结束，执行重置函数
-        {
+            this.style.backgroundColor = "blue";
             reset();
+            arguments = null;
+            setTimeout(bind(find,this),500);
+            //this.style.backgroundColor = "white";
         }
+        else{
+            this.style.backgroundColor = "yellow";
+            this.mvisited = true;
+
+            if (this.nextSibling && this.id != "super") {
+                setTimeout(query(last,this.nextSibling), 500);  //0.5s后遍历右子树（兄弟节点）
+            }
+            if (!this.firstElementChild && !this.nextSibling || this.firstChild.data == "bike" || this.firstChild.data == "program" || this.id == "super")
+            {
+                setTimeout(query(removeColor,this),500);  //为某些不方便改变背景色的节点硬绑定removeColor函数
+            }
+            if (this.previousSibling && this.previousSibling.mvisited && !this.nextSibling || this.parentNode.childNodes.length == 2) {
+                setTimeout(query(last,this.parentNode), 500); //当该节点为最底层右子树时，0.5s后遍历父节点
+            }
+            if(this.id == "super")//遍历结束，执行重置函数
+            {
+                reset();
+            }
+        }
+
     }
 }
 
@@ -57,3 +87,4 @@ function reset(){
 reset();
 
 document.getElementById("ergodic").addEventListener("click",bind(last,document.getElementById("super")));
+document.getElementById("query").addEventListener("click",query(last,document.getElementById("super")));
